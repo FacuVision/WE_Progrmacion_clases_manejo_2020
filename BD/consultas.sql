5051f3f2323c23f7927e6a9f672919949fd3792b
FacuVision <egarayar@autonoma.edu.pe>
2020-09-17 17:34:56 -0500

    nuevo commit

---
+++ BD/consultas.sql
@@ -0,0 +1,17 @@
+INSERT INTO `alumno`(`alum_nombre`, `alum_apellido`, `alum_correo`, `alum_telefono`, `alum_estado_pago`, `alum_estado`) VALUES (
+'Jose', 'Rodriguez','jose@gmail.com','852369741','correcto','habilitado');
+
+INSERT INTO `alumno`(`alum_nombre`, `alum_apellido`, `alum_correo`, `alum_telefono`, `alum_estado_pago`, `alum_estado`) VALUES (
+'Jose', 'Rodriguez','jose@gmail.com','852369741','correcto','habilitado');
+
+INSERT INTO `alumno`(`alum_nombre`, `alum_apellido`, `alum_correo`, `alum_telefono`, `alum_estado_pago`, `alum_estado`) VALUES (
+'Jose', 'Rodriguez','jose@gmail.com','852369741','correcto','habilitado');
+
+INSERT INTO `alumno`(`alum_nombre`, `alum_apellido`, `alum_correo`, `alum_telefono`, `alum_estado_pago`, `alum_estado`) VALUES (
+'Jose', 'Rodriguez','jose@gmail.com','852369741','correcto','habilitado');
+
+INSERT INTO `alumno`(`alum_nombre`, `alum_apellido`, `alum_correo`, `alum_telefono`, `alum_estado_pago`, `alum_estado`) VALUES (
+'Jose', 'Rodriguez','jose@gmail.com','852369741','correcto','habilitado');
+
+INSERT INTO `alumno`(`alum_nombre`, `alum_apellido`, `alum_correo`, `alum_telefono`, `alum_estado_pago`, `alum_estado`) VALUES (
+'Jose', 'Rodriguez','jose@gmail.com','852369741','correcto','habilitado');
