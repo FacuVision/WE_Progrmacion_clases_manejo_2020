410e05f0807c88215b4baf32d82a00934d319851
FacuVision <egarayar@autonoma.edu.pe>
2020-10-06 12:49:01 -0500

    Cambios en la BD
    Se modifico algunas sentencias a la nueva BD de la escuela de manejo

--- DAO/AgnoDAO.php
+++ DAO/AgnoDAO.php
@@ -9,7 +9,7 @@ class AgnoDAO{
         $numero_agno = $AgnoBean->getAgno();
 
         $instanciacompartida = ConexionBD::getInstance();
-        $sql = "INSERT INTO agno(agno_numero) VALUES ('$numero_agno')";
+        $sql = "INSERT INTO agnos(agno_numero) VALUES ('$numero_agno')";
         $estado = $instanciacompartida->EjecutarConEstado($sql);
 
         $AgnoBean->setId_agno($instanciacompartida->Ultimo_ID());
@@ -27,7 +27,7 @@ class AgnoDAO{
 
         //SE EJECUTARA 12 VECES 
         for ($numero_mes=1; $numero_mes < 13; $numero_mes++) { 
-            $sql = "INSERT INTO mes(id_agno,mes_numero) VALUES ($agno_id,'$numero_mes')";
+            $sql = "INSERT INTO meses(id_agno,mes_numero) VALUES ($agno_id,'$numero_mes')";
             $estado2 = $instanciacompartida->EjecutarConEstado($sql);
         }
         
@@ -40,7 +40,7 @@ class AgnoDAO{
         $id_agno = $AgnoBean->getId_agno();
 
         $instanciacompartida = ConexionBD::getInstance();
-        $sql = "DELETE FROM agno WHERE id_agno=$id_agno";
+        $sql = "DELETE FROM agnos WHERE id_agno=$id_agno";
         $estado = $instanciacompartida->EjecutarConEstado($sql);
         
         return $estado;
